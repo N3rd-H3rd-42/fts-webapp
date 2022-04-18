646ab71870e4444edb0d0bc15b17939cd2c6163d
Josh McLain <joshmclain45@gmail.com>
2022-04-17 20:50:31 -0700

    ejs files

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const testing = 'pre commit';
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, () => console.log('webapp running'));
+app.get('/', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/next', (req, res) => {
+  return res.render('secondpage');
+});
+
+app.listen(PORT, () => console.log(`webapp running ${PORT}`));
